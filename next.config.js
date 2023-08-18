8693d56cab7787ef36833b5dfbc1bb16b9233871
Hassaan Ahmed <codinghassaan@gmail.com>
2023-08-18 16:07:38 +0500

    .

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   images: {
     domains: [
       "firebasestorage.googleapis.com",
     ],
-}
+},
+eslint:false,
 }
 
 module.exports = nextConfig
